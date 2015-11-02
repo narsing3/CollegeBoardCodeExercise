95a209883c50c3d7583b7d87d2a2735317495485
narsing3 <narsing.lingala@gmail.com>
2015-11-02 11:51:51 -0600

    Initial Commit
    College Board Code Excercise

---
+++ src/com/collegeboard/narsing/codingexcersise/BinarySearchBean.java
@@ -0,0 +1,95 @@
+package com.collegeboard.narsing.codingexcersise;
+
+/**
+ * @author Narsing
+ *
+ * Bean class acts as a VO for encapsulating the search details. 
+ */
+public class BinarySearchBean {
+	int pivot;
+	int left; 
+	int right;
+	int attemptCount;
+	String userReply;
+	double MAX_COUNT;
+	/**
+	 * @return the pivot
+	 */
+	public int getPivot() {
+		return pivot;
+	}
+	/**
+	 * @param pivot the pivot to set
+	 */
+	public void setPivot(int pivot) {
+		this.pivot = pivot;
+	}
+	/**
+	 * @return the left
+	 */
+	public int getLeft() {
+		return left;
+	}
+	/**
+	 * @param left the left to set
+	 */
+	public void setLeft(int left) {
+		this.left = left;
+	}
+	/**
+	 * @return the right
+	 */
+	public int getRight() {
+		return right;
+	}
+	/**
+	 * @param right the right to set
+	 */
+	public void setRight(int right) {
+		this.right = right;
+	}
+	/**
+	 * @return the attemptCount
+	 */
+	public int getAttemptCount() {
+		return attemptCount;
+	}
+	/**
+	 * @param attemptCount the attemptCount to set
+	 */
+	public void setAttemptCount(int attemptCount) {
+		this.attemptCount = attemptCount;
+	}
+	
+	public void incrementAttemptCount(){
+		setAttemptCount(getAttemptCount()+1);
+	}
+	/**
+	 * @return the userReply
+	 */
+	public String getUserAnswer() {
+		return userReply;
+	}
+	/**
+	 * @param userReply the userReply to set
+	 */
+	public void setUserAnswer(String userAnswer) {
+		this.userReply = userAnswer;
+	}
+	
+	/**
+	 * @return the mAX_COUNT
+	 */
+	public double getMAX_COUNT() {
+		return MAX_COUNT;
+	}
+	/**
+	 * @param mAX_COUNT the mAX_COUNT to set
+	 */
+	public void setMAX_COUNT(double mAX_COUNT) {
+		MAX_COUNT = mAX_COUNT;
+	}
+	public String toString (){
+		return (" Pivot:"+getPivot()+"  left:"+getLeft()+"  right:"+getRight()+"  attemptCount:"+getAttemptCount());
+	}
+}
---
+++ src/com/collegeboard/narsing/codingexcersise/Constants.java
@@ -0,0 +1,24 @@
+package com.collegeboard.narsing.codingexcersise;
+
+/**
+ * 
+ * @author Narsing
+ * This class avoids hard coding values across the app.
+ */
+public class Constants {
+	
+
+	public static final String HIGHER = "HIGHER";
+	public static final String LOWER = "LOWER";
+	public static final String YES = "YES";
+	public static final String END = "END";
+	public static final String READY = "READY";
+	
+	public static final  int MIN_NUMBER = 0;
+	public static final  int MAX_NUMBER = 1000;
+	public static final  int INIT_PIVOT = -1;
+	public static final  int INIT_ATTEMPTCOUNT = 0;
+	public static final  double INIT_MAXATTEMPTS = 0.0;
+	
+	
+}
